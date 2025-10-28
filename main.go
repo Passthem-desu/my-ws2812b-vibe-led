7fdb4d2a37be3022ea2beb3427c31c9ed1de03b1
passthem <Passthem183@gmail.com>
2025-10-28 22:41:05 +0800

    允许指定参数，方便兼容更多设备

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 )
 
-const SPIDevicePath = "/dev/spidev1.0"
-const APIPort = 8080
-
 func main() {
+	spiDevice := flag.String("spi", "/dev/spidev1.0", "SPI 设备路径");
+	apiPort := flag.Int("port", 8080, "Web API 监听端口");
+
 	log.Printf("启动 LED Shader API 服务")
 	
-	controller, err := NewController(SPIDevicePath)
+	controller, err := NewController(*spiDevice)
 	if err != nil {
 		log.Fatalf("无法初始化 SPI 控制器: %v", err)
 	}
@@ -24,7 +25,7 @@ func main() {
 	
 	router := setupRouter(pipeline)
 	
-	log.Printf("Web API 监听在 :%d...", APIPort)
-	router.Run(fmt.Sprintf(":%d", APIPort))
+	log.Printf("Web API 监听在 :%d...", *apiPort)
+	router.Run(fmt.Sprintf(":%d", *apiPort))
 }
 
